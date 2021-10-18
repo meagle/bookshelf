6e299381a75f0745f0420c7ec064515b9b0be4dc
Mark Eagle <meagle@gmail.com>
2021-10-17 20:50:01 -0400

    02-styles completed exercise changes with styled

--- src/components/lib.exercise.js
+++ src/components/lib.exercise.js
@@ -4,6 +4,50 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 // 🐨 create a button styled component here called "Button"
 // make it look nice and allow it to support a "variant" prop which can be
 // either "primary" or "secondary".
+const Button = styled.button(props => {
+  return {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+    background:
+      props.variant === 'primary'
+        ? '#3f51b5'
+        : props.variant === 'secondary'
+        ? '#f1f2f7'
+        : 'white',
+    color:
+      props.variant === 'primary'
+        ? 'white'
+        : props.variant === 'secondary'
+        ? '#434449'
+        : 'white',
+  }
+})
+
+// This is what KCD does instead.  I did it the way I did above
+// because that was what I understood from the readme instructions.
+// However, I like this approach better
+/* const buttonVariants = {
+  primary: {
+    background: '#3f51b5',
+    color: 'white',
+  },
+  secondary: {
+    background: '#f1f2f7',
+    color: '#434449',
+  },
+}
+const Button = styled.button(
+  {
+    padding: '10px 15px',
+    border: '0',
+    lineHeight: '1',
+    borderRadius: '3px',
+  },
+  ({variant = 'primary'}) => buttonVariants[variant],
+) */
+
 // 💰 don't forget to export it at the bottom!
 // 💰 In my final version, I style padding, border, lineHeight, and borderRadius
 //    the same for both types, and then change the background and color based
@@ -30,15 +74,17 @@ import {Dialog as ReachDialog} from '@reach/dialog'
 //   background: '#f1f2f7',
 //   color: '#434449',
 
-// Input
-//   borderRadius: '3px',
-//   border: '1px solid #f1f1f4',
-//   background: '#f1f2f7',
-//   padding: '8px 12px',
+const Input = styled.input({
+  borderRadius: '3px',
+  border: '1px solid #f1f1f4',
+  background: '#f1f2f7',
+  padding: '8px 12px',
+})
 
-// FormGroup
-//   display: 'flex',
-//   flexDirection: 'column',
+const FormGroup = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+})
 
 // 💰 I'm giving a few of these to you:
 const CircleButton = styled.button({
@@ -68,4 +114,4 @@ const Dialog = styled(ReachDialog)({
   },
 })
 
-export {CircleButton, Dialog}
+export {Button, CircleButton, Dialog, Input, FormGroup}
