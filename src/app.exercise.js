4f934bc69af6d68ced5429e023096211d1d5e30d
Mark Eagle <meagle@gmail.com>
2022-01-03 14:29:51 -0500

    04-authentication EC03 solution

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,12 +3,12 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import {FullPageSpinner} from './components/lib'
 import * as colors from './styles/colors'
 import {client} from './utils/api-client'
 import {useAsync} from './utils/hooks'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
-import {FullPageSpinner} from 'components/lib'
 
 async function getUser() {
   let user = null
@@ -23,21 +23,20 @@ async function getUser() {
 }
 
 function App() {
-  // const [user, setUser] = React.useState(null)
   const {
     data: user,
     error,
-    isIdle,
     isLoading,
-    isSuccess,
+    isIdle,
     isError,
+    isSuccess,
     run,
     setData,
   } = useAsync()
 
   React.useEffect(() => {
     run(getUser())
-  }, [])
+  }, [run])
 
   const login = form => auth.login(form).then(user => setData(user))
   const register = form => auth.register(form).then(user => setData(user))
@@ -46,27 +45,35 @@ function App() {
     setData(null)
   }
 
-  return isLoading || isIdle ? (
-    <FullPageSpinner />
-  ) : isError ? (
-    <div
-      css={{
-        color: colors.danger,
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <p>Uh oh... There's a problem. Try refreshing the app.</p>
-      <pre>{error.message}</pre>
-    </div>
-  ) : isSuccess && user ? (
-    <AuthenticatedApp user={user} logout={logout} />
-  ) : (
-    <UnauthenticatedApp login={login} register={register} />
-  )
+  if (isLoading || isIdle) {
+    return <FullPageSpinner />
+  }
+
+  if (isError) {
+    return (
+      <div
+        css={{
+          color: colors.danger,
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <p>Uh oh... There's a problem. Try refreshing the app.</p>
+        <pre>{error.message}</pre>
+      </div>
+    )
+  }
+
+  if (isSuccess) {
+    return user ? (
+      <AuthenticatedApp user={user} logout={logout} />
+    ) : (
+      <UnauthenticatedApp login={login} register={register} />
+    )
+  }
 }
 
 export {App}
--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,5 +1,5 @@
+import {logout} from '../auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
-
 function client(
   endpoint,
   {token, headers: customHeaders, ...customConfig} = {},
@@ -13,6 +13,13 @@ function client(
   }
 
   return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      console.log('401 repsonse encountered')
+      await logout()
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please log back in and try again'})
+    }
+
     const data = await response.json()
     if (response.ok) {
       return data
