7ce2e819d68bb164b867d9acc6a1ab86d37f90d6
Mark Eagle <meagle@gmail.com>
2022-01-03 14:10:06 -0500

    04-authentication EC02 solution

--- src/app.exercise.js
+++ src/app.exercise.js
@@ -3,48 +3,66 @@ import {jsx} from '@emotion/core'
 
 import * as React from 'react'
 import * as auth from 'auth-provider'
+import * as colors from './styles/colors'
 import {client} from './utils/api-client'
+import {useAsync} from './utils/hooks'
 import {AuthenticatedApp} from './authenticated-app'
 import {UnauthenticatedApp} from './unauthenticated-app'
+import {FullPageSpinner} from 'components/lib'
+
+async function getUser() {
+  let user = null
+
+  const token = await auth.getToken()
+  if (token) {
+    const data = await client('me', {token})
+    user = data.user
+  }
+
+  return user
+}
 
 function App() {
-  const [user, setUser] = React.useState(null)
-
-  const getUser = async () => {
-    let user = null
-    const token = await auth.getToken()
-    if (token) {
-      console.log('token', token)
-      // we're logged in! Let's go get the user's data:
-      // await client('me', {token}).then(data => {
-      //   console.log(data.user)
-      //   user = data.user
-      // })
-      user = await client('me', {token})
-    }
-    return user
-  }
+  // const [user, setUser] = React.useState(null)
+  const {
+    data: user,
+    error,
+    isIdle,
+    isLoading,
+    isSuccess,
+    isError,
+    run,
+    setData,
+  } = useAsync()
 
   React.useEffect(() => {
-    getUser().then(data => {
-      console.log('data:', data)
-      setUser(data.user)
-    })
+    run(getUser())
   }, [])
 
-  const login = form =>
-    auth.login(form).then(u => {
-      setUser(u)
-      // return this value so it can be retrived by the aync hook and set in 'data'
-      return u
-    })
-  const register = form => auth.register(form).then(u => setUser(u))
+  const login = form => auth.login(form).then(user => setData(user))
+  const register = form => auth.register(form).then(user => setData(user))
   const logout = () => {
     auth.logout()
-    setUser(null)
+    setData(null)
   }
 
-  return user ? (
+  return isLoading || isIdle ? (
+    <FullPageSpinner />
+  ) : isError ? (
+    <div
+      css={{
+        color: colors.danger,
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <p>Uh oh... There's a problem. Try refreshing the app.</p>
+      <pre>{error.message}</pre>
+    </div>
+  ) : isSuccess && user ? (
     <AuthenticatedApp user={user} logout={logout} />
   ) : (
     <UnauthenticatedApp login={login} register={register} />
